002a4cfaffaa8143189a562e83325b51e3052dda
ChetanaMali <65480069+ChetanaMali@users.noreply.github.com>
2020-09-30 23:03:55 +0530

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -5,13 +5,13 @@ from __future__ import unicode_literals
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
-import os
-import sys
+import os                                          #import operating system
+import sys                                         # import system
 sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'https://www.vianna.de'
-RELATIVE_URLS = False
+RELATIVE_URLS = False                            
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
